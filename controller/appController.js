b480134dd3b7769a906bb714e518544012f81648
NibidaGhimire <nibidaghimire39@gmail.com>
2023-07-14 14:37:21 +0545

    edit

--- controller/appController.js
+++ controller/appController.js
@@ -1,73 +1,3 @@
-// const nodemailer = require('nodemailer');
-// const gm = require('gm').subClass({ imageMagick: true });
-// const fs = require('fs');
-
-
-// const { EMAIL, PASSWORD } = require('../env.js')
-
-
-
-
-// const certificate = (req, res) => {
-//     const imageName = './cert.png'; 
-//     const name = req.body.name || 'Nibida Ghimire'; 
-
-//     gm(imageName)
-//         .font('Arial', 40)
-//         .fill('black')
-//         .drawText(0, 0, name, 'center')
-//         .gravity('Center')
-//         .toBuffer('png', (err, buffer) => {
-//             if (err) {
-//                 console.error(err);
-//                 res.status(500).send('An error occurred');
-//             }
-//             fs.writeFileSync('output.png', buffer);
-
-
-//             const { userEmail } = req.body;
-
-//             let config = {
-//                 service: 'gmail',
-//                 auth: {
-//                     user: EMAIL,
-//                     pass: PASSWORD
-//                 }
-//             }
-
-//             let transporter = nodemailer.createTransport(config);
-
-//             let message = {
-//                 from: EMAIL,
-//                 to: userEmail,
-//                 subject: "Certificate",
-//                 html: `Please find the image attached.<br><br>Best regards,<br>Nibida Ghimire`,
-//                 attachments: [
-//                     {
-//                         filename: 'Certificate.png',
-//                         path: "output.png"
-//                         // content: buffer
-
-//                     }
-//                 ]
-//             }
-
-//             transporter.sendMail(message).then(() => {
-//                 return res.status(201).json({
-//                     msg: "you should receive an email"
-//                 })
-//             }).catch(error => {
-//                 return res.status(500).json({ error })
-//             })
-//         });
-
-// }
-
-
-// module.exports = {
-//     certificate
-// }
-
 
 const nodemailer = require('nodemailer');
 const { createCanvas, loadImage } = require('canvas');
@@ -79,7 +9,7 @@ const { EMAIL, PASSWORD } = require('../env.js')
 const certificate = async (req, res) => {
   const { userEmail } = req.body;
   const imageName = 'cert.png';
-  const name = req.body.name || 'Nibida Ghimire';
+  const name = req.body.name ;
 
   try {
     const image = await loadImage(imageName);
